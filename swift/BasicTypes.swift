b3daf10b575aec3822cf88f13c7aace6f3c36650
Prem Kamble <premkamble099@gmail.com>
2025-05-28 14:50:38 +0530

    Create BasicTypes.swift

---
+++ swift/BasicTypes.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+// String is "reguler text"
+let myFirstItem: String = "Hello, World!"
+
+// Refrence previously created objects
+let myTitle = myFirstItem
+
+//Boolean (aka 'bool' - True or False
+
+let mySecondItem: Bool = true
+let myThirdItem: Bool = false
+
+//Do not do !
+//let myFourthItem: String = "true"
+
+// Swift is type-safe language
+let myFifthItem: Bool = true
+let mySixthItem: String = "Hello World!"
+let mySeventhItem = "hello world!"
+
+
+// Date
+let myFirstDate: Date = Date()
+ 
+// Number can be int Double CGfloa & more..
+// int is the whole integer
+let myFirstNUmber: Int = 1
+// Double fo Math
+let mySecondNumber: Double = 1.0
+// CGFloat is for UI
+let myThirdNumber: CGFloat = 1.0
