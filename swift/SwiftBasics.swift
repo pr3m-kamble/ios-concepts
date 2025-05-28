3d15c2529ac81fd517b7a171f1a4d428d2c63a48
Prem Kamble <premkamble099@gmail.com>
2025-05-28 13:51:24 +0530

    Create SwiftBasics.swift

---
+++ swift/SwiftBasics.swift
@@ -0,0 +1,40 @@
+
+import Foundation
+
+var greeting = "Hello, playground"
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+
+//commnets
+
+// this is single line comment
+
+/*
+ this is multiline comment
+ ...
+ ...
+ */
+
+// Naming Conventions
+ 
+
+// Camel Case - CORRECT!!
+// the first word is lowercased and then first charecter in every following world is uppercased.
+let firstGreeting = "Hello World!"
+let thisIsMyFirstGreeting = "Hello World!"
+
+
+//WRONG
+let thisismyfirstgreeting = "Hello World!"
+
+// Sanke Case - WRONG!
+let this_is_my_first_greeting = "Hello World!"
+
+// Pascal case - WRONG!
+let ThisIsMyFirstGreeting = "Hello World!"
+
+
+
