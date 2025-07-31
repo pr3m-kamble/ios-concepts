02e157afe9d5297d57a5fdd1f8c34c2ef877d142
Prem Kamble <premkamble099@gmail.com>
2025-07-31 23:06:15 +0530

    Create initializer.swift

---
+++ SwiftUI-Basic/initializer.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+struct InitializerBootcamp: View {
+    
+    let backgroundColor: Color
+    let count: Int
+    let title: String
+    
+    init(count: Int, fruit: Fruit){
+       
+        self.count = count
+        if fruit == .apple {
+            self.title = "Apples"
+            self.backgroundColor = .red
+        }else {
+            self.title = "Oranges"
+            self.backgroundColor = .orange
+        }
+    }
+    
+    enum Fruit{
+        case apple
+        case ornage
+    }
+    
+    var body: some View {
+        VStack(spacing: 12){
+            Text("\(count)")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .underline()
+            
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.white)
+        }
+        .frame(width: 150, height: 150)
+        .background(backgroundColor)
+        .cornerRadius(30)
+    }
+}
+
+#Preview {
+    HStack{
+        InitializerBootcamp(count: 100, fruit: .apple)
+        InitializerBootcamp(count: 60, fruit: .ornage)
+    }
+    
+}
