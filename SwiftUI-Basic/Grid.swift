f6c057373f87d6bb843cd2c7867ad127e5a01e20
Prem Kamble <premkamble099@gmail.com>
2025-08-01 23:36:00 +0530

    Create Grid.swift

---
+++ SwiftUI-Basic/Grid.swift
@@ -0,0 +1,65 @@
+import SwiftUI
+
+struct GridBootcamp: View {
+    let columns : [GridItem] = [
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+//        GridItem(.fixed(75), spacing: nil, alignment: nil),
+//        GridItem(.fixed(100), spacing: nil, alignment: nil),
+//        GridItem(.fixed(75), spacing: nil, alignment: nil),
+//        GridItem(.fixed(50), spacing: nil, alignment: nil),
+        
+    ]
+    var body: some View {
+        ScrollView {
+                        Rectangle()
+                            .fill(.orange)
+                            .frame(height: 400)
+            
+            LazyVGrid(columns: columns,
+                      alignment: .center,
+                      spacing: 6,
+                      pinnedViews: [.sectionHeaders],
+                      content: {
+                Section(header:
+                            Text("Section 1")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(Color.blue)
+                            .padding()
+                ) {
+                    ForEach(0..<20) { index in
+                        Rectangle()
+                            
+                            .frame(height: 150)
+                    }
+                }
+                Section(header:
+                            Text("Section 1")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(Color.red)
+                            .padding()
+                ) {
+                    ForEach(0..<20) { index in
+                        Rectangle()
+                            .fill(Color.green)
+                            .frame(height: 150)
+                    }
+                }
+                })
+            
+        
+                
+                
+            }
+        }
+    }
+
+
+#Preview {
+    GridBootcamp()
+}
