f72788c7708568a3103c713876e63a6fc2d35bc2
Prem Kamble <premkamble099@gmail.com>
2026-02-08 23:04:35 +0530

    Add TextSelection example in TextSelaction.swift

---
+++ SwiftUI-Basic/TextSelaction.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct TextSelactionBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelactionBootcamp()
+}
