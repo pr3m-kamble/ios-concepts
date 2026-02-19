f1a41ad4ddb05180fb5e50ca9234c5202d1c9249
Prem Kamble <premkamble099@gmail.com>
2026-02-19 22:53:35 +0530

    Add SafeAreaInsetBootcamp view
    Implement SafeAreaInsetBootcamp view with navigation and safe area insets.

---
+++ SwiftUI-Basic/afeAreaInset.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+struct SafeAreaInsetBootcamp: View {
+    var body: some View {
+        NavigationStack {
+            List(0..<10) { _ in
+                Rectangle()
+                    .frame(height: 300)
+            }
+            .navigationTitle("Safe Area Insets")
+            .navigationBarTitleDisplayMode(.inline)
+//            .overlay(
+//                Text("Hi")
+//                    .frame(maxWidth: .infinity)
+//                    .background(Color.yellow)
+//
+//                ,alignment: .bottom
+//            )
+            .safeAreaInset(edge: .top, alignment: .trailing, spacing: nil) {
+                Text("Hi")
+                    .frame(maxWidth: .infinity)
+//                    .padding()
+                    .background(Color.yellow.edgesIgnoringSafeArea(.bottom))
+//                    .clipShape(Circle())
+//                    .padding()
+            }
+            .safeAreaInset(edge: .bottom, alignment: .trailing, spacing: nil) {
+                Text("Hi")
+                    .frame(maxWidth: .infinity)
+//                    .padding()
+                    .background(Color.yellow.edgesIgnoringSafeArea(.bottom))
+//                    .clipShape(Circle())
+//                    .padding()
+            }
+        }
+    }
+}
+
+
+#Preview {
+    SafeAreaInsetBootcamp()
+}
