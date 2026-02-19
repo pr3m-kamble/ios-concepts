db96c398f1ff3c8ead2bdc7904533275b8ec7e41
Prem Kamble <premkamble099@gmail.com>
2026-02-19 23:52:28 +0530

    Create NativePopover.swift

---
+++ SwiftUI-Basic/NativePopover.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+
+struct NativePopoverBootcamp: View {
+    
+    @State private var showPopover: Bool = false
+    @State private var feedbackOptions: [String] = [
+        "Very good 🥳",
+        "Average 🙂",
+        "Very bad 😡"
+    ]
+    
+    var body: some View {
+        ZStack {
+            Color.gray.ignoresSafeArea()
+            
+            VStack {
+                
+                Spacer()
+
+                Button("Provide feedback?") {
+                    showPopover.toggle()
+                }
+                .padding(20)
+                .background(Color.yellow)
+                .popover(isPresented: $showPopover, attachmentAnchor: .point(.top), content: {
+                    ScrollView {
+                        VStack(alignment: .leading, spacing: 12, content: {
+                            ForEach(feedbackOptions, id: \.self) { option in
+                                Button(option) {
+                                    
+                                }
+                                
+                                if option != feedbackOptions.last {
+                                    Divider()
+                                }
+                            }
+                        })
+                        .padding(20)
+                    }
+                    .presentationCompactAdaptation(.popover)
+                })
+            }
+        }
+    }
+}
+
+
+#Preview {
+    NativePopoverBootcamp()
+}
