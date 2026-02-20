ec5ac3ad5719e8f861cd7e16aebecac92da5a20d
Prem Kamble <premkamble099@gmail.com>
2026-02-21 00:10:53 +0530

    Add ContentUnavailableViewBootcamp for iOS 17

---
+++ SwiftUI-Basic/ontentUnavailableView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct ContentUnavailableViewBootcamp: View {
+    var body: some View {
+        if #available(iOS 17.0, *) {
+//            ContentUnavailableView.search(text: "abc")
+            ContentUnavailableView(
+                "No Internet Connection",
+                systemImage: "wifi.slash",
+                description: Text("Please connect to the internet and try again.")
+            )
+        } else {
+            // Fallback on earlier versions
+            // FakeContentUnavailableView()
+        }
+    }
+}
+
+#Preview {
+    ContentUnavailableViewBootcamp()
+}
